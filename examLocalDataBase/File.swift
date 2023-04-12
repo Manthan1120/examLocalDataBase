854bc8ca346e2e43ba5bdd47ec1f66a3396c6f19
AniketDhandhukiya <addhandhukiya2309@gmail.com>
2023-04-12 11:16:09 +0530

    Mathan

---
+++ examLocalDataBase/File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  examLocalDataBase
+//
+//  Created by R83 on 12/04/23.
+//
+
+import Foundation
+import SQLite3
+
+struct Data {
+    var id: Int
+    var name: String
+}
+class Sqlite {
+    static var file = OpaquePointer?
+    
+    static func
+}
